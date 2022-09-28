793dc3bf64e6d587bf4c9d1a426562841f30fabc
Vitlaii Novokshonov <arkam.raw@gmail.com>
2022-09-28 07:50:19 +0300

    api-intagrate

---
+++ config.py
@@ -0,0 +1,4 @@
+import telebot
+from newsapi import NewsApiClient
+bot = telebot.TeleBot('5398193760:AAE3dN32J3k-Q-OPOoxbDzual7_CUhwhEXM')
+newsapi = NewsApiClient(api_key='3bda45d8aa22460bb91edc10ecbcb34b')
